d40c3973791f056b869de777ddab358407b564d6
Siddharth Mathur <siddharth@getfastah.com>
2017-06-11 12:22:59 +0530

    Fix copyright text

--- examples/app/src/main/java/com/getfastah/exampleswithfastahnetworkkit/DemoAudioVideoPreloadActivity.java
+++ examples/app/src/main/java/com/getfastah/exampleswithfastahnetworkkit/DemoAudioVideoPreloadActivity.java
@@ -1,5 +1,5 @@
 /*
- * Copyright $today.year, Blackbuck Computing Inc.
+ * Copyright 2017, Blackbuck Computing Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
